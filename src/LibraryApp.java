338a28fb4238786a3c92321c1381184f9584fb43
gale <gale.a.chapman@gmail.com>
2018-10-25 15:03:15 -0400

    removed variables

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -4,6 +4,13 @@ public class LibraryApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		String userName;
+		int checkReturn;
+		int recomSearch;
+		String bookTitle;
+		String keyWord;
+		
 	}
 
 }
