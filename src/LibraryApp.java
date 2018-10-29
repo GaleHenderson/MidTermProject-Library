a034bdd2e397493aff542df792199c5d76b67c98
reeclark <reeclark@icloud.com>
2018-10-29 17:14:14 -0400

    added return function

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -31,105 +31,135 @@ public class LibraryApp {
 
 		System.out.println("Welcome to the Grand Circus Library!");
 		System.out.println("What would you like to do today!?");
-		int userPick = LabValidator.getInt(scan, "\nPress 1 to check out a book or 2 to return a book:  ", 1, 2);
 		int userChoice = 0;
 
-		if (userPick == 1) {
-			do {
-				System.out.println("1 - See list of books");
-				System.out.println("2 - Search by the author");
-				System.out.println("3 - Search by the title ");
-				System.out.println("4 - Return to main menu");
-
-				userChoice = LabValidator.getInt(scan, "\nEnter menu number:  ", 1, 4);
-				if (userChoice == 1) {
-					books = readFromFile(directoryFolder, fileName);
-					int counter = 1;
-					for (Book book : books) {
-
-						System.out.println(counter++ + ". " + book);
-					}
+				do {
+					System.out.println("1 - See list of books");
+					System.out.println("2 - Search by the author");
+					System.out.println("3 - Search by the title");
+					System.out.println("4 - Return a book");
+					System.out.println("5 - Donate a book");
+					System.out.println("6 - Exit program");
 					
-				
-					
-					String user1st = LabValidator.getString(scan, "\nWould you like to check out a book? Y/N  ");
-					while (user1st.equalsIgnoreCase("y")) {
-						int userChoiceForBookNumber = LabValidator.getInt(scan,
-								"Please select the number that corresponds to the book you'd like to check out:  ", 1,
-								13);
-						for (int i = 0; i < books.size() + 1; i++) {
-							if (userChoiceForBookNumber == i) {
-								System.out.println("\nYou've checked out " + books.get(i - 1) + ".");
-								writeNewStatusToFile(books, books.get(i - 1));
-								System.out.println(
-										books.get(i - 1).getTitle() + " is now " + books.get(i - 1).getStatus());
-								System.out.println("This book is due back in 2 weeks.\n");
-								System.out.println("Would you like to continue? Y/N  ");
-								user1st = scan.next();
+					userChoice = LabValidator.getInt(scan, "\nEnter menu number:  ", 1, 4);
+					if (userChoice == 1) {
+						books = readFromFile(directoryFolder, fileName);
+						int counter = 1;
+						for (Book book : books) {
+							System.out.println(counter++ + ". " + book);
+						}
+						String user1st = LabValidator.getString(scan, "\nWould you like to check out a book? Y/N  ");
+						while (user1st.equalsIgnoreCase("y")) {
+							int userChoiceForBookNumber = LabValidator.getInt(scan,
+									"Please select the number that corresponds to the book you'd like to check out:  ",
+									1, 13);
+							for (int i = 0; i < books.size() + 1; i++) {
+								if (userChoiceForBookNumber == i) {
+									System.out.println("\nYou've checked out " + books.get(i - 1) + ".");
+									writeNewStatusToFile(books, books.get(i - 1));
+									System.out.println(
+											books.get(i - 1).getTitle() + " is now " + books.get(i - 1).getStatus());
+									System.out.println("This book is due back in 2 weeks.\n");
+									System.out.println("Would you like to continue? Y/N  ");
+									user1st = scan.next();
 
-								if (user1st.equalsIgnoreCase("N")) {
-									System.out.println("Thank you for choosing Grand Circus Library. Goodbye");
-
+									if (user1st.equalsIgnoreCase("N")) {
+										System.out.println("Thank you for choosing Grand Circus Library. Goodbye");
+									}
 								}
-
 							}
 						}
-
-					}
-
-				} else if (userChoice == 2) {
-					searchForAuthorOfBook(scan, readFromFile(directoryFolder, fileName));
-
-				} else if (userChoice == 3) {
-					SearchForTitleOfBook(scan, readFromFile(directoryFolder, fileName));
-				} else if (userChoice == 4) {
-					int userCheckout = LabValidator.getInt(scan, "Pick a book to checkout from the list: ", 1, 12);
-
-				}
-			} while (!(userChoice == 5));
-		} else if (userPick == 2) {
-			String userReturnAuthor = LabValidator.getString(scan, "Enter the Author's name");
-			String userReturnTitle = LabValidator.getString(scan, "Enter the title of the book");
-			Path filePath = Paths.get(directoryFolder, fileName);
-			File file = filePath.toFile();
-			try {
-				String checkout = "checkout";
-				Book s = new Book(userReturnTitle, userReturnAuthor, checkout, "2 weeks");
-				PrintWriter outW = new PrintWriter(new FileOutputStream(file, true));
-				outW.println(s);
-				outW.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+					} else if (userChoice == 2) {
+						searchForAuthorOfBook(scan, readFromFile(directoryFolder, fileName));
+					} else if (userChoice == 3) {
+						SearchForTitleOfBook(scan, readFromFile(directoryFolder, fileName));
+					} else if (userChoice == 4) {
+						returnBook(scan, readFromFile(directoryFolder, fileName));
+					} else if (userChoice == 5) {
+						String userDonateAuthor = LabValidator.getString(scan, "Enter the Author's name");
+						String userDonateTitle = LabValidator.getString(scan, "Enter the title of the book");
+						Path filePath = Paths.get(directoryFolder, fileName);
+						File file = filePath.toFile();
+						try {
+							String checkout = "checkout";
+							Book s = new Book(userDonateTitle, userDonateAuthor, checkout, "2 weeks");
+							PrintWriter outW = new PrintWriter(new FileOutputStream(file, true));
+							outW.println(s);
+							outW.close();
+						} catch (FileNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					} 
+				} while (!(userChoice == 6));
+		} 
+	
+	public static void returnBook(Scanner scan, ArrayList<Book> bookArrayList) {
+		String userSelection = LabValidator.getString(scan, "Please enter the name of title or author.");
+		int available = 0;
+		for (Book book : bookArrayList) {
+			if (((book.getAuthor().contains(userSelection.toUpperCase()) || (book.getTitle().contains(userSelection.toUpperCase()))))
+					&& (book.getStatus().contains("CHECKED OUT"))) {
+				available = available + 1;
 			}
+			if (available == 1) {
+				writeOldStatusToFile(bookArrayList, book);
+				System.out.println("Thank you for returning this book!");
+				break;
+			}
+			}
+		if (available < 1) {
+			System.out.println("Sorry that book is checked out or is not apart of our inventory.");
+			System.out.println("What else would you like to do?");
 		}
+	}
+	
+	public static void writeOldStatusToFile(ArrayList<Book> books, Book book) {
+		String directoryFolder = "library";
+		String fileName = "library.txt";
+		Path filePath = Paths.get(directoryFolder, fileName);
+		File file = filePath.toFile();
+		book.setStatus("ON SHELF");
+
+		try {
+			// the true parameter for the file output stream constructor appends data
+			// to the end of the file. False rewrites over the entire file
+			PrintWriter outW = new PrintWriter(new FileOutputStream(file));
+			for (Book b : books) {
+				outW.println(b.getTitle() + "," + b.getAuthor() + "," + b.getStatus() + "," + b.getDueDate());
+			}
+			outW.close(); // mandatory: this needs to be closed when done or may not write all info
+		} catch (FileNotFoundException e) {
+			System.out.println("The file was not found.");
+		}
+
 	}
 
 	public static void searchForAuthorOfBook(Scanner scan, ArrayList<Book> bookArrayList) {
-        String isItOnShelf = "ON SHELF";
+		String isItOnShelf = "ON SHELF";
 		String userSelection = LabValidator.getString(scan, "Please enter the name of the author.");
 		int available = 0;
 		for (Book book : bookArrayList) {
-			if ((book.getAuthor().contains(userSelection.toUpperCase()) && (book.getStatus().contains(isItOnShelf)))) { // added the toUpperCase to make sure user
-																			// input will be understood by the search
+			if ((book.getAuthor().contains(userSelection.toUpperCase()) && (book.getStatus().contains(isItOnShelf)))) { 
 				System.out.println(book);
 				available = available + 1;
 			}
-			if (available ==1) {
+			if (available == 1) {
 
-				
-				String userCheckout = LabValidator.getString(scan, "Would you like to checkout this book? (Yes/y or No/n)");
+				String userCheckout = LabValidator.getString(scan,
+						"Would you like to checkout this book? (Yes/y or No/n)");
 				if (userCheckout.equalsIgnoreCase("yes") || userCheckout.equalsIgnoreCase("y")) {
-					System.out.println("Congrats! You have checked out " + book.getTitle() + " by " + book.getAuthor() + " for 2 weeks.");
-					writeNewStatusToFile(bookArrayList , book);
+					System.out.println("Congrats! You have checked out " + book.getTitle() + " by " + book.getAuthor()
+							+ " for 2 weeks.");
+					writeNewStatusToFile(bookArrayList, book);
 					System.out.println("What else would you like to do?");
-					
+
 					break;
 				} else {
 					System.out.println("What else would you like to do?");
 				}
 			}
-			
+
 		}
 
 		if (available < 1) {
@@ -137,7 +167,7 @@ public class LibraryApp {
 			System.out.println("What else would you like to do?");
 
 		}
-		
+
 	}
 
 	public static void SearchForTitleOfBook(Scanner scan, ArrayList<Book> searchForTitle) {
@@ -151,25 +181,27 @@ public class LibraryApp {
 				available = available + 1;
 			}
 			if (available == 1) {
-				String userCheckout = LabValidator.getString(scan, "Would you like to checkout this book? (Yes/y or No/n)");
+				String userCheckout = LabValidator.getString(scan,
+						"Would you like to checkout this book? (Yes/y or No/n)");
 				if (userCheckout.equalsIgnoreCase("yes") || userCheckout.equalsIgnoreCase("y")) {
-					System.out.println("Congrats! You have checked out " + book.getTitle() + " by " + book.getAuthor() + " for 2 weeks.");
-					writeNewStatusToFile(searchForTitle , book);
-					
+					System.out.println("Congrats! You have checked out " + book.getTitle() + " by " + book.getAuthor()
+							+ " for 2 weeks.");
+					writeNewStatusToFile(searchForTitle, book);
+
 					System.out.println("What else would you like to do?");
 					break;
 				} else {
 					System.out.println("What else would you like to do?");
 				}
 			}
-			
+
 		}
 
 		if (available < 1) {
 			System.out.println("Sorry that book is checked out or  is not apart of our inventory.");
 			System.out.println("What else would you like to do?");
 		}
-		}
+	}
 
 	public static void createFile(String directoryFolder, String fileName) {
 		Path filePath = Paths.get(directoryFolder, fileName);
@@ -219,14 +251,13 @@ public class LibraryApp {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void writeNewStatusToFile(ArrayList<Book> books, Book book) {
 		String directoryFolder = "library";
 		String fileName = "library.txt";
 		Path filePath = Paths.get(directoryFolder, fileName);
 		File file = filePath.toFile();
 		book.setStatus("CHECKED OUT");
-
 
 		try {
 			// the true parameter for the file output stream constructor appends data
@@ -277,5 +308,5 @@ public class LibraryApp {
 		}
 		return bookList;
 	}
-	
+
 }
